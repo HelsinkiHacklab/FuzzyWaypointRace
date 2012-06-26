cbf627aa7e0fc9bc03d20a683089c1b36c2d7d57
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-06-27 01:30:26 +0300

    bearing calculation based on the haversine equations

--- pocclient/geopy_test.py
+++ pocclient/geopy_test.py
@@ -5,6 +5,16 @@ with open(os.path.realpath(__file__).replace('geopy_test.py', 'pocclient.yml')) 
 
 import geopy, geopy.distance
 
+
+#Horisontal Bearing
+def calcBearing(lat1, lon1, lat2, lon2):
+    from math import *
+    dLon = lon2 - lon1
+    y = sin(dLon) * cos(lat2)
+    x = cos(lat1) * sin(lat2) \
+        - sin(lat1) * cos(lat2) * cos(dLon)
+    return degrees(atan2(y, x)+pi)
+
 numpoints = len(config['waypoints'])
 for i1 in range(numpoints):
     for i2 in range(numpoints):
@@ -19,5 +29,5 @@ for i1 in range(numpoints):
         dv = geopy.distance.VincentyDistance(gp1, gp2)
         dg = geopy.distance.GreatCircleDistance(gp1, gp2)
         
-        print "%s <-> %s VincentyDistance: %s" % (p1['title'], p2['title'], dv)
-        print "%s <-> %s GreatCircleDistance: %s" % (p1['title'], p2['title'], dg)
+        print "%s <-> %s VincentyDistance: %skm to %fdeg" % (p1['title'], p2['title'], dv.km, calcBearing(gp1[0],gp1[1],gp2[0],gp2[1]))
+        print "%s <-> %s GreatCircleDistance: %skm to %fdeg" % (p1['title'], p2['title'], dg.km, calcBearing(gp1[0],gp1[1],gp2[0],gp2[1]))
