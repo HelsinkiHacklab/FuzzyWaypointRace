bbfeb62c122da24d7cb9469a176bf0d915002224
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-07-02 23:04:27 +0300

    just to note that explicit sets work no better

--- pocclient/pocclient.py
+++ pocclient/pocclient.py
@@ -92,6 +92,8 @@ class CircleLayer(gobject.GObject, osmgpsmap.GpsMapLayer):
             c = gtk.gdk.color_parse(circle[3])
             print "circle[3]: %s repr(c)=%s" % (circle[3], repr(c))
             ggc = drawable.new_gc(c,c)
+            gcc.set_foreground(c)
+            gcc.set_background(c)
             ggc.set_line_attributes(self.config['distance_line_width'], ggc.line_style, ggc.cap_style, ggc.join_style)
             # TODO: There probably is a better way to calculate these but I don't care, this works now
             drawable.draw_arc(ggc, False, view_wp[0], view_np[1], (view_ep[0] - view_wp[0]), (view_sp[1] - view_np[1]), 0, 360*64)
